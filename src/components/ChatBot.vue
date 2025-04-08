03183b91c967d57f41e689044ea19cdb48946df7
howon-ryu <qorgh2akfl@naver.com>
2025-04-08 11:08:42 +0900

    pull merge 후cors 파트 다시 커밋

--- src/components/ChatBot.vue
+++ src/components/ChatBot.vue
@@ -54,7 +54,7 @@ export default {
         async getBotResponse(userMessage) {
             // 여기에 실제 API 엔드포인트를 입력하세요
             // const apiUrl = 'http://211.254.213.18:30000/items';
-            const apiUrl = 'http://127.0.0.1:8000/items';
+            const apiUrl = 'http://211.254.213.18:8000/items';
 
             const response = await fetch(apiUrl, {
                 method: 'POST',
